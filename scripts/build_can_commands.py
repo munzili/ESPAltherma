1f86cf6dc36457cc9b584cc15c849aaf974b558d
munzili <munzili@users.noreply.github.com>
2023-04-15 20:22:52 +0200

    support generating of more command files

--- scripts/build_can_commands.py
+++ scripts/build_can_commands.py
@@ -15,34 +15,49 @@ if not os.path.exists(buildDir):
    os.makedirs(buildDir)
 
 files_to_convert = []
-files_to_convert.extend(glob.glob(os.path.join(data_src_dir, 'commands_hpsu_*.json')))
+files_to_convert.extend(glob.glob(os.path.join(data_src_dir, '*.json')))
+
+language_directorys = [os.path.abspath(os.path.join(data_src_dir,name)) for name in os.listdir(os.path.join(data_src_dir, '.')) if os.path.isdir(os.path.join(data_src_dir, name))]
 
 print('  files to convert: ' + str(files_to_convert))
-
-commandsFile = open(data_src_dir + "commands_hpsu.json")
-commands = json.load(commandsFile)["commands"]
-commandsFile.close()
+print('  directorys to convert: ' + str(language_directorys))
 
 for file in files_to_convert:
-    print('  Converting file: ' + file)
-    srcFile = os.path.join(data_src_dir, os.path.basename(file))
-    targetFile = os.path.join(buildDir, os.path.basename(file))
+    print('  Converting file: ' + os.path.basename(file))
 
-    languageFile = open(srcFile, "r", encoding='utf8')
-    languageDefinition = json.load(languageFile)
-    languageFile.close()
+    commandsFile = open(file, "r", encoding='utf8')
+    commands = json.load(commandsFile)["commands"]
+    commandsFile.close()
 
-    counter = 0
-    for definition in languageDefinition["Commands"]:
-        defName = definition["name"]
-        command = commands[defName]
+    for langDirectory in language_directorys:
+        languageFilePath = os.path.join(langDirectory, os.path.basename(file))
 
-        for c in command:
-            languageDefinition["Commands"][counter][c] = command[c]
+        if not os.path.isfile(languageFilePath):
+            continue
 
-        counter += 1
+        print('  Converting file: ' + languageFilePath)
+        languageFile = open(languageFilePath, "r", encoding='utf8')
+        languageDefinition = json.load(languageFile)
+        languageFile.close()
 
-    with open(targetFile, "w", encoding='utf8') as outfile:
-        json.dump(languageDefinition, outfile, ensure_ascii=False)
+        srcFile = os.path.join(data_src_dir, os.path.basename(file))
+        targetFile = os.path.join(buildDir, os.path.basename(file))
+        languageFileDir = os.path.join(buildDir, languageDefinition["Language"])
+
+        if not os.path.exists(languageFileDir):
+            os.makedirs(languageFileDir)
+
+        counter = 0
+        for definition in languageDefinition["Commands"]:
+            defName = definition["name"]
+            command = commands[defName]
+
+            for c in command:
+                languageDefinition["Commands"][counter][c] = command[c]
+
+            counter += 1
+
+        with open(os.path.join(languageFileDir, os.path.basename(file)), "w", encoding='utf8') as outfile:
+            json.dump(languageDefinition, outfile, ensure_ascii=False)
 
 print('Finished generating CAN language files!')
